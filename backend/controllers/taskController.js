761f3085d94781d8fdc013b986c1221633e05850
Michael Goberman <michaelgoberman8@gmail.com>
2026-04-03 15:10:45 +1000

    implemented get tasks function

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,8 +1,22 @@
+const Task = require('../models/Task');
+
 //this function adds a task to the database.
 const addTask = async(req, res) => {
     const {title, description} = req.body;
 
     try {
         const task = await Task.create({userId: req.user.id, title, description, deadline});
+        res.status(201).json(task);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+// this function fetches all tasks related to a user
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({userId: req.user.id});
+    } catch (error){
+        res.status(500).json({message: error.message});
     }
 }
