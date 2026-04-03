0e479247a1c83e3b979fd6ee38a3ec0cec46799c
Michael Goberman <michaelgoberman8@gmail.com>
2026-04-03 15:15:08 +1000

    implemented update task function

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -2,12 +2,32 @@ const Task = require('../models/Task');
 
 //this function adds a task to the database.
 const addTask = async(req, res) => {
-    const {title, description} = req.body;
+    const {title, description, deadline} = req.body;
 
     try {
         const task = await Task.create({userId: req.user.id, title, description, deadline});
         res.status(201).json(task);
     } catch (error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+// this function updates an existing task with new information.
+const updateTask = async(req, res) => {
+    const {title, description, completed, deadline} = req.body;
+
+    try {
+        const task = await Task.findById(req.params.id);
+        if(!task) return res.status(404).json({message: 'Task not found'});
+
+        task.title = title || task.title;
+        task.description = description || task.description;
+        task.completed = completed || task.completed;
+        task.deadline = deadline || task.deadline;
+
+        const updatedTask = await task.save();
+        res.json(updatedTask);
+    } catch (error) {
         res.status(500).json({message: error.message});
     }
 }
