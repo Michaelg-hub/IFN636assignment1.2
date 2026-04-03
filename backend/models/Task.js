3f46fc7a2aeb2a8e78c0a4264f5798d1f6a23bb9
Michael Goberman <michaelgoberman8@gmail.com>
2026-04-03 14:45:10 +1000

    task schema additions

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     title: { type: String, required: true, unique: true },
     description: { type: String, required: true}
 });
+
+module.exports = mongoose.model('Task', taskSchema);
