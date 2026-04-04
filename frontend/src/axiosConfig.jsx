1b59dadbd6b368906ba8c0f89db8afce43bae82c
Michael Goberman <michaelgoberman8@gmail.com>
2026-04-05 09:16:29 +1000

    axiosConfig ip update

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.252.174.104:5001', // live
+  baseURL: 'http://13.210.48.52:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
